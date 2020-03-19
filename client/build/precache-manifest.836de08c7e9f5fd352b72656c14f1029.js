96f41f89cd0e38d7f32501234678f3a7f7443bc5
Saramoshggoo <smoshggoo@gmail.com>
2020-03-18 17:02:47 -0700

    initial commit

---
+++ client/build/precache-manifest.836de08c7e9f5fd352b72656c14f1029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a4c6d6d8630e47df81dfd900f0b2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181f56dc1c04cd2df867",
+    "url": "/static/css/main.08957180.chunk.css"
+  },
+  {
+    "revision": "2978c0ed975b0ce9805d",
+    "url": "/static/js/2.a5b19e94.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a5b19e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181f56dc1c04cd2df867",
+    "url": "/static/js/main.a093b427.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
